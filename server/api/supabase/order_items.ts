e39043251b60b82001c4ef763dd987d1105274f6
Gabriel Wilkins <wilkigab001@gmail.com>
2025-09-18 20:16:59 -0600

    Making a few Updates

---
+++ server/api/supabase/order_items.ts
@@ -0,0 +1,32 @@
+import {createClient} from '@supabase/supabase-js';
+import {defineEventHandler, createError} from 'h3';
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string, 
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string 
+)
+
+export default defineEventHandler(async (event) => {
+    try{
+        const {data, error} = await supabase
+        .from('order_items')
+        .select('*')
+
+        if(error){
+            throw createError({
+                statusCode: 500,
+                statusMessage: error.message
+            })
+        }
+
+        return {
+            success: true,
+            data: data
+        }
+    }catch(e: any){
+        return {
+            success: false,
+            message: e.message || 'Unexpected error occurred'
+        }
+    }
+})
